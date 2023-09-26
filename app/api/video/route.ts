22a763442f12b26cdd1b2471fdc86c7cc624de4f
deepsolankii <deep99solanki@gmail.com>
2023-09-26 14:18:36 +0530

    added UI to show free generation

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import Replicate from "replicate";
+import { checkApiLimit, increaseApiLimit } from "@/lib/api-limit";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN!,
@@ -18,6 +19,10 @@ export async function POST(req: Request) {
     if (!prompt) {
       return new NextResponse("Prompt is required", { status: 400 });
     }
+    const freeTrial = await checkApiLimit();
+    if (!freeTrial) {
+      return new NextResponse("Free Trial expired.", { status: 403 });
+    }
     const response = await replicate.run(
       "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
       {
@@ -26,6 +31,7 @@ export async function POST(req: Request) {
         },
       }
     );
+    await increaseApiLimit();
     return NextResponse.json(response);
   } catch (err) {
     console.log("Video error", err);
